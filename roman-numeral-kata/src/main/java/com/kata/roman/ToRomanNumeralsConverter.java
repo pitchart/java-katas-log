b244582c6b3a23ea12642eadaac74d4bb83d0821
HARABIDA <elmehdi.harabida.partner@decathlon.com>
2023-05-03 10:15:52 +0200

    add loop

--- roman-numeral-kata/src/main/java/com/kata/roman/ToRomanNumeralsConverter.java
+++ roman-numeral-kata/src/main/java/com/kata/roman/ToRomanNumeralsConverter.java
@@ -2,11 +2,12 @@ package com.kata.roman;
 
 import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class ToRomanNumeralsConverter {
 
-    private static final Map<Integer, String> ARABIC_TO_ROMAN_MAP = Map.of(
+    private static final TreeMap<Integer,String> ARABIC_TO_ROMAN_MAP = new TreeMap<>(Map.of(
             1000, "M",
             500, "D",
             100, "C",
@@ -15,12 +16,22 @@ public class ToRomanNumeralsConverter {
             5, "V",
             1, "I",
             0, ""
-    );
+    ));
+
 
     public String convert(int arabic) {
 
+
 //        ARABIC_TO_ROMAN_MAP.entrySet().stream()
 //                .collect(Collectors.toList());
+
+
+
+        if(arabic == 0) return "";
+
+        for(Integer key : ARABIC_TO_ROMAN_MAP.keySet()){
+            if (arabic >= key) return ARABIC_TO_ROMAN_MAP.get(key) + convert(arabic - key);
+        }
 
         if (arabic >= 1000) return ARABIC_TO_ROMAN_MAP.get(1000) + convert(arabic - 1000);
         if (arabic >= 500) return ARABIC_TO_ROMAN_MAP.get(500) + convert(arabic - 500);
